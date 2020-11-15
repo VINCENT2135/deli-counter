ce8fae95092299b606d173c5d14e66d48c145873
Vincent Ivos <vincentivos1@gmail.com>
2020-11-15 17:56:30 -0500

    commit tree 4bb4eec7fb3ab2f520df4162f4648ffe7496b31a

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
+katz_deli = []
+
+def line(katz_deli)
+ s="The line is currently: "
+if katz_deli.length == 0
+     puts "The line is currently empty."
+ else
+    katz_deli.each_with_index do |value,i|
+    # puts "The numbline is currently: #{katz_deli.size}."
+    s = s + "#{i + 1}. #{value} "
+   end
+   puts s.strip!
+ end
+end
+
+def take_a_number(katz, name)
+   katz.push(name)
+   puts "Welcome, #{name}. You are number #{katz.size} in line."
+end
+
+def now_serving(katz)
+  if katz.size == 0
+      puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz.first}."
+    return katz.shift
+  end
+end
+
+
 # Write your code here.
